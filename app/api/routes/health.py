35bbe58719bf696bc8b5eb5c3f707fdf1f8b77a5
carolina-museum <carolinaminamioguchi@gmail.com>
2024-10-18 16:16:19 -0700

    return float and remove unnecessary function

--- app/api/routes/health.py
+++ app/api/routes/health.py
@@ -1,13 +1,12 @@
 # health information
 
 import os
-import uuid
 
 import chromadb
 from fastapi import APIRouter
 from openai import OpenAI
 
-from app.models import LearnRequest, BMIRequest, BMIResponse
+from app.models import BMIRequest, BMIResponse
 
 
 openai_client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
@@ -30,19 +29,7 @@ router = APIRouter()
 
 @router.post("/bmi", response_model=BMIResponse, status_code=200)
 def bmi(bmi_request: BMIRequest):
-    print("request received")
     weight = bmi_request.weight
     height = bmi_request.height
     bmi = round(weight / ((height / 100) ** 2), 2)
-    print(bmi)
-    bmi_result_message = "Your BMI is " + str(bmi) + "."
-    return BMIResponse(message=bmi_result_message)
-
-
-@router.post("/learn", status_code=204)
-def learn(learn_request: LearnRequest):
-    learn_collection.add(
-        documents=[learn_request.text],
-        ids=[uuid.uuid4().hex],
-    )
-    return
+    return BMIResponse(bmi=bmi)
