c3877ff4c1428171f75beb180fa2764db5b57709
Kazuki Kijima <kazukiyoshida96@gmail.com>
2024-10-18 19:40:33 -0700

    :white_check_mark: Add a test example for BMI API

--- app/models.py
+++ app/models.py
@@ -13,12 +13,12 @@ class LearnRequest(BaseModel):
     text: str = Field(description="The text to be learned by the chatbot")
 
 
-# Practice
 class BMIRequest(BaseModel):
+    # FIXME: Add min and max values to the fields
+    # FIXME: Add a description about units (cm, kg)
     weight: float = Field(description="Weight value requested to calculate BMI.")
     height: float = Field(description="Height value requested to calculate BMI.")
 
 
-# Practice
 class BMIResponse(BaseModel):
     bmi: float = Field(description="BMI value to be responded by the chatbot")
