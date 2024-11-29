e371141f9762f0bbc17d44fc5b59e0287ff94bb3
carolina-museum <carolinaminamioguchi@gmail.com>
2024-11-29 00:45:03 -0800

    made modifications based on review

--- scripts/evaluate_chunk_size.py
+++ scripts/evaluate_chunk_size.py
@@ -20,22 +20,14 @@ chromadb_client = chromadb.HttpClient(
 )
 
 openai_client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
-chromadb_client = chromadb.HttpClient(
-    host=os.getenv("CHROMADB_HOST", "chromadb"),
-    port=8000,
-    ssl=False,
-    headers=None,
-    settings=chromadb.config.Settings(),
-    tenant=chromadb.config.DEFAULT_TENANT,
-    database=chromadb.config.DEFAULT_DATABASE,
-)
+
 
 # Questions to ask about the text
 f_query = open("../data/RAG_chunk_evaluation_questions.txt", "r")
 my_queries = f_query.read().split("\n")
 f_query.close()
 
-unique_file_id = str(int(time.time()))
+timestamp = str(int(time.time()))
 
 for chunk_size_desc in chunk_sizes:
 
@@ -43,47 +35,45 @@ for chunk_size_desc in chunk_sizes:
 
     # a file to store responses
     save_path = "evaluate_chunk_size_results"
-    if not os.path.exists(save_path):
-        os.makedirs(save_path)
-    f_response = open(
-        save_path + "/response_" + unique_file_id + "_" + chunk_size_desc + ".txt", "w"
-    )
+    os.makedirs(save_path, exist_ok=True)
+    f_response = open(f"{save_path}/response_{timestamp}_{chunk_size_desc}.txt", "w")
     f_response.writelines([chunk_size_desc, "\n"])
 
     # read and chunk the text
     f = open("../data/chapterXX.txt", "r")
-    chunk_sizes = ["paragraph", "half_a_paragraph", "quarter_a_paragraph", "sentence"]
+
     if chunk_size_desc == "paragraph":
         my_documents = f.read().split("\n")
         my_ids = [f"{uuid.uuid4().hex}" for _ in range(len(my_documents))]
+
     elif chunk_size_desc == "half_a_paragraph":
         my_documents_tmp = f.read().split("\n")
         my_documents = []
-        for i in range(
-            len(my_documents_tmp)
-        ):  # Computationally heavy. Let's look for a python library.
-            for c in range(2):
-                half = len(my_documents_tmp[i]) // 2
-                my_documents.append(my_documents_tmp[i][c * half : (c + 1) * half])
+        # Computationally heavy. Let's look for a python library.
+        for i, chunk_tmp in enumerate(my_documents_tmp):
+            for j in range(2):
+                half = len(chunk_tmp) // 2
+                my_documents.append(chunk_tmp[j * half : (j + 1) * half])
         my_ids = [f"{uuid.uuid4().hex}" for _ in range(len(my_documents))]
+
     elif chunk_size_desc == "quarter_a_paragraph":
         my_documents_tmp = f.read().split("\n")
         my_documents = []
-        for i in range(
-            len(my_documents_tmp)
-        ):  # Computationally heavy. Let's look for a python library.
-            for c in range(4):
-                quarter = len(my_documents_tmp[i]) // 4
-                my_documents.append(
-                    my_documents_tmp[i][c * quarter : (c + 1) * quarter]
-                )
+        # Computationally heavy. Let's look for a python library.
+        for i, chunk_tmp in enumerate(my_documents_tmp):
+            for j in range(4):
+                quarter = len(chunk_tmp) // 4
+                my_documents.append(chunk_tmp[j * quarter : (j + 1) * quarter])
         my_ids = [f"{uuid.uuid4().hex}" for _ in range(len(my_documents))]
+
     elif chunk_size_desc == "sentence":
         my_documents = f.read().split(".")
         my_ids = [f"{uuid.uuid4().hex}" for _ in range(len(my_documents))]
+
     else:  # split by space if nothing is specified. Just in case.
         my_documents = f.read().split(" ")
         my_ids = [f"{uuid.uuid4().hex}" for _ in range(len(my_documents))]
+
     f.close()
 
     # store in the database
@@ -103,8 +93,7 @@ for chunk_size_desc in chunk_sizes:
     )
 
     # ask questions
-    my_queries_index = [i for i in range(len(my_queries))]
-    for q, i in zip(my_queries, my_queries_index):
+    for i, q in enumerate(my_queries):
 
         rag_result = learn_collection.query(
             query_texts=[q],
@@ -141,7 +130,7 @@ for chunk_size_desc in chunk_sizes:
         f_response.writelines([response.choices[0].message.content, "\n"])
 
         # show progress
-        print(str(i + 1) + "/" + str(len(my_queries)))
+        print(f"{i+1}/{len(my_queries)}")
 
     f_response.close()
 
